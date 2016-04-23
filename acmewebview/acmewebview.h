01b7cc3a537458bc812a68aa7a50f1d6ba0b4c6e
Mike De Wolfe <dewolfe.michael@gmail.com>
2016-04-23 15:13:24 -0400

    Initial Commit

---
+++ acmewebview/acmewebview.h
@@ -0,0 +1,19 @@
+//
+//  acmewebview.h
+//  acmewebview
+//
+//  Created by Michael De Wolfe on 2016-04-23.
+//  Copyright © 2016 acmethunder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for acmewebview.
+FOUNDATION_EXPORT double acmewebviewVersionNumber;
+
+//! Project version string for acmewebview.
+FOUNDATION_EXPORT const unsigned char acmewebviewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <acmewebview/PublicHeader.h>
+
+
---
+++ acmewebviewTests/acmewebviewTests.m
@@ -0,0 +1,39 @@
+//
+//  acmewebviewTests.m
+//  acmewebviewTests
+//
+//  Created by Michael De Wolfe on 2016-04-23.
+//  Copyright © 2016 acmethunder. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface acmewebviewTests : XCTestCase
+
+@end
+
+@implementation acmewebviewTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
